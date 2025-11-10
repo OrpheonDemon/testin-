14e4a1d42253bbb27aee49185b04c8f6abb5fd3e
OrpheonDemon <nachobocangelo@gmail.com>
2025-11-10 17:58:48 -0400

    Update actualizarEstado.php

--- Backend/actualizarEstado.php
+++ Backend/actualizarEstado.php
@@ -1,10 +1,10 @@
 <?php
     require_once 'conexion.php';
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+   // use PHPMailer\PHPMailer\PHPMailer;
+    //use PHPMailer\PHPMailer\Exception;
 
-    require 'C:/xampp/htdocs/TecnologiaWebII/ProyectoFinal/vendor/autoload.php';
+    //require 'C:/xampp/htdocs/TecnologiaWebII/ProyectoFinal/vendor/autoload.php';
 
     $idUsuario = $_POST['IdUsuario'];
 
@@ -20,32 +20,32 @@
             $to = $user['Correo'];
             $nombre = $user['Nombres'];
 
-            $mail = new PHPMailer(true);
-            try {
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'bibliohospi1999@gmail.com';
-                $mail->Password = 'pgfk gzzw izxr borc'; 
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 587;
+            //$mail = new PHPMailer(true);
+            //try {
+              //  $mail->isSMTP();
+               // $mail->Host = 'smtp.gmail.com';
+               // $mail->SMTPAuth = true;
+                //$mail->Username = 'bibliohospi1999@gmail.com';
+                //$mail->Password = 'pgfk gzzw izxr borc'; 
+                //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                //$mail->Port = 587;
 
-                $mail->setFrom('bibliohospi1999@gmail.com', 'Biblioteca del Hsopital');
-                $mail->addAddress($to, $nombre);
+                //$mail->setFrom('bibliohospi1999@gmail.com', 'Biblioteca del Hsopital');
+                //$mail->addAddress($to, $nombre);
 
-                $mail->isHTML(true);
-                $mail->Subject = 'Usuario Habilitado en la Biblioteca del Hospital';
-                $mail->Body = "Hola <b>$nombre</b>,<br><br>
-                          Tu cuenta ha sido <b>habilitada</b> por el administrador. 
-                          Ahora puedes iniciar sesión y usar el sistema.<br><br>
-                          Saludos,<br>Equipo de Biblioteca del Hospital";
-                $mail->AltBody="Hola $nombre,\n\nTu cuenta ha sido habilitada por el administrador. Ahora puedes iniciar sesión.\n\nSaludos,\nEquipo de Biblioteca del Hospital";
+                //$mail->isHTML(true);
+                //$mail->Subject = 'Usuario Habilitado en la Biblioteca del Hospital';
+               // $mail->Body = "Hola <b>$nombre</b>,<br><br>
+                 //         Tu cuenta ha sido <b>habilitada</b> por el administrador. 
+                  //        Ahora puedes iniciar sesión y usar el sistema.<br><br>
+                  //        Saludos,<br>Equipo de Biblioteca del Hospital";
+               // $mail->AltBody="Hola $nombre,\n\nTu cuenta ha sido habilitada por el administrador. Ahora puedes iniciar sesión.\n\nSaludos,\nEquipo de Biblioteca del Hospital";
 
-                $mail->send();
+                //$mail->send();
 
-            } catch (Exception $e){
-                echo "No se pudo enviar el correo. Error: {$mail->ErrorInfo}";
-            }
+           // } catch (Exception $e){
+             //   echo "No se pudo enviar el correo. Error: {$mail->ErrorInfo}";
+            //}
 
             header("Location: http://localhost:8080/TecnologiaWebII/ProyectoFinal/Fronted/comprobar_user.php?msg=aprobado");
             exit;
